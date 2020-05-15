c4cfe8c1a328a4bd9b4ff77c995410e97c657568
Lucas Lin <linlucas33@gmail.com>
2020-05-14 23:47:18 -0700

    Finished class Account for banking.py

--- banking.py
+++ banking.py
@@ -1,3 +1,14 @@
-class account:
-    def __init__(self):
-        se
+class Account:
+    def __init__(self, first='', last='', nationality='', balance=0):
+        self.first_name = first
+        self.last_name = last
+        self.nationality = nationality
+        self.balance = balance
+
+    def __repr__(self):
+        return 'Name: {} {}\nNationality: {}\nBalance: {}'\
+            .format(self.first_name, self.last_name, self.nationality, self.balance)
+
+
+lucas = Account()
+print(lucas)
